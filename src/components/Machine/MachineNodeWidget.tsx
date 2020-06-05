e2d138029b7dc6a34f96651303cfaaffa78e998b
Matthew Jeorrett <Matthew@wallet.services>
2020-06-05 16:36:49 +0100

    add popup controls for machine

--- src/components/Machine/MachineNodeWidget.tsx
+++ src/components/Machine/MachineNodeWidget.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { DiagramEngine } from '@projectstorm/react-diagrams';
 import styled from '@emotion/styled';
 
@@ -8,12 +8,26 @@ import { MachineWidgetBase } from '../MachineWidgetBase';
 import { MachineNodeModel } from './MachineNodeModel';
 import { MachinePortWidget } from './MachinePortWidget';
 
+type SControlsProps = {
+  isOpen: boolean,
+};
 
 const S = {
   Ports: styled.div`
     border-top: 1px solid darkolivegreen;
     display: flex;
     padding: 0.25rem;
+  `,
+  Controls: styled.div<SControlsProps>`
+    background: #eeffdd;
+    border-radius: 2px;
+    padding: 0.5em;
+    transform: scaleY(${p => p.isOpen ? 1 : 0});
+    transform-origin: bottom;
+    transition: transform 0.2s ease-out;
+  `,
+  ControlDropdown: styled.select`
+    width: 100%;
   `,
   IngredientPorts: styled.div``,
   PortsSpacer: styled.div`
@@ -32,27 +46,45 @@ export const MachineNodeWidget: React.SFC<MachineNodeWidgetProps> = ({
   engine,
   node,
 }) => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <MachineWidgetBase
-      iconName={node.machineName}
-      isSelected={node.isSelected()}
-      tooltipText={getLabel(node.machineName)}
-    >
-      {Object.keys(node.getPorts()).length > 0 && (
-        <S.Ports>
-          <S.IngredientPorts>
-            {node.ingredientPorts.map(port => (
-              <MachinePortWidget key={port.getName()} engine={engine} port={port} />
-            ))}
-          </S.IngredientPorts>
-          <S.PortsSpacer />
-          <S.ResultPorts>
-            {node.resultPorts.map(port => (
-              <MachinePortWidget key={port.getName()} engine={engine} port={port} />
-            ))}
-          </S.ResultPorts>
-        </S.Ports>
-      )}
-    </MachineWidgetBase>
+    <div onMouseEnter={() => setIsOpen(true)} onMouseLeave={() => setIsOpen(false)}>
+      <S.Controls isOpen={isOpen}>
+        <S.ControlDropdown>
+          <option>Test option 1</option>
+          <option>Test option 1</option>
+          <option>Test option 1</option>
+          <option>Test option 1</option>
+        </S.ControlDropdown>
+        <S.ControlDropdown>
+          <option>Test option 1</option>
+          <option>Test option 1</option>
+          <option>Test option 1</option>
+          <option>Test option 1</option>
+          <option>Test option 1</option>
+        </S.ControlDropdown>
+      </S.Controls>
+      <MachineWidgetBase
+        iconName={node.machineName}
+        isSelected={node.isSelected()}
+        tooltipText={getLabel(node.machineName)}
+      >
+        {Object.keys(node.getPorts()).length > 0 && (
+          <S.Ports>
+            <S.IngredientPorts>
+              {node.ingredientPorts.map(port => (
+                <MachinePortWidget key={port.getName()} engine={engine} port={port} />
+              ))}
+            </S.IngredientPorts>
+            <S.PortsSpacer />
+            <S.ResultPorts>
+              {node.resultPorts.map(port => (
+                <MachinePortWidget key={port.getName()} engine={engine} port={port} />
+              ))}
+            </S.ResultPorts>
+          </S.Ports>
+        )}
+      </MachineWidgetBase>
+    </div>
   );
 };
