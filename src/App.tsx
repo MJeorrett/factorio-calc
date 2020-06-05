327ac2a0876d1471767506dda3d892974a8dd5f8
Matthew Jeorrett <Matthew@wallet.services>
2020-06-05 10:51:43 +0100

    style title

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import styled from '@emotion/styled';
 
 import * as C from './components';
 import { diagramEngine } from './diagramEngine'
 
+const S = {
+  Title: styled.h1`
+    left: 0;
+    position: absolute;
+    text-align: center;
+    top: 0;
+    width: 100%;
+  `,
+}
+
 function App() {
   return (
     <>
-      <h1>Factorio Calc</h1>
+      <S.Title>Factorio Calc</S.Title>
       <C.Canvas engine={diagramEngine} />
     </>
   );
